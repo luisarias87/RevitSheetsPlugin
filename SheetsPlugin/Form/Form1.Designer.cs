74399868088bf7c31b8ea16dd7b4745337c86a49
luis arias <ariasluisla89@gmail.com>
2022-08-29 19:20:54 -0500

    Added Levels

--- SheetsPlugin/Form/Form1.Designer.cs
+++ SheetsPlugin/Form/Form1.Designer.cs
@@ -28,34 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.BtnSheets = new System.Windows.Forms.Button();
+            this.DuplicateViewsDataGridView = new System.Windows.Forms.DataGridView();
+            this.ColCheck = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.DuplicateViewsDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
-            // BtnSheets
+            // DuplicateViewsDataGridView
             // 
-            this.BtnSheets.Location = new System.Drawing.Point(124, 128);
-            this.BtnSheets.Name = "BtnSheets";
-            this.BtnSheets.Size = new System.Drawing.Size(202, 68);
-            this.BtnSheets.TabIndex = 0;
-            this.BtnSheets.Text = "Sheets Test";
-            this.BtnSheets.UseVisualStyleBackColor = true;
-            this.BtnSheets.Click += new System.EventHandler(this.BtnSheets_Click);
+            this.DuplicateViewsDataGridView.AllowUserToAddRows = false;
+            this.DuplicateViewsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DuplicateViewsDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColCheck});
+            this.DuplicateViewsDataGridView.Location = new System.Drawing.Point(122, 77);
+            this.DuplicateViewsDataGridView.Name = "DuplicateViewsDataGridView";
+            this.DuplicateViewsDataGridView.RowHeadersWidth = 51;
+            this.DuplicateViewsDataGridView.RowTemplate.Height = 24;
+            this.DuplicateViewsDataGridView.Size = new System.Drawing.Size(297, 200);
+            this.DuplicateViewsDataGridView.TabIndex = 0;
+            // 
+            // ColCheck
+            // 
+            this.ColCheck.HeaderText = "";
+            this.ColCheck.MinimumWidth = 6;
+            this.ColCheck.Name = "ColCheck";
+            this.ColCheck.Width = 125;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(486, 338);
-            this.Controls.Add(this.BtnSheets);
+            this.Controls.Add(this.DuplicateViewsDataGridView);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DuplicateViewsDataGridView)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button BtnSheets;
+        private System.Windows.Forms.DataGridView DuplicateViewsDataGridView;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn ColCheck;
     }
 }
